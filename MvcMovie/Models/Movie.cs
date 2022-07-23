9d40b7702aaae3d1d03aa206f01b249c7af4cf8b
Сергей Охлопков <sokhlopkovs@gmail.com>
2022-07-24 01:00:04 +0300

    Изучены миграции. Внесены правки в представления

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -17,7 +17,8 @@ namespace MvcMovie.Models
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
-        public decimal Price { get; set; } 
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
 
     }
 
